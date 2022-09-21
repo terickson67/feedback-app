c6059d1a0802cf28173d7c1e999eaef13e86ae4e
terickson67 <terickson@cyberhusky.io>
2022-09-21 18:00:27 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+function App () {
+    const title = 'Blog Post'
+    const body = 'This is my blog post'
+    const comments = [
+        {id: 1, text: 'comment one'},
+        {id: 2, text: 'comment two'},
+        {id: 3, text: 'comment three'}
+    ]
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const loading = false
+    const showComments = true
+    const commentBlock = (<div className='comments'>
+    <h3>Comments ({comments.length})</h3>
+        <ul>
+            {comments.map((comment, index) =>
+            <li key={index}>{comment.text}</li>
+            )}
+        </ul>
+</div>)
+
+    if(loading) return <h1>Loading. . .</h1>
+
+    return (
+        <div className='container'>
+            <h1>{title.toUpperCase()}</h1>
+            <p>{body}</p>
+
+            {showComments && (commentBlock)}
+        </div>
+    )
 }
 
-export default App;
+export default App
