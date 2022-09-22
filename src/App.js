a5cc78ddb5d6d296d899c6481945640eab254467
Taylor Erickson <terickson@cyberhusky.io>
2022-09-21 21:08:19 -0400

    This is my first commit after pulling the repo

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
-function App () {
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-    const comments = [
-        {id: 1, text: 'comment one'},
-        {id: 2, text: 'comment two'},
-        {id: 3, text: 'comment three'}
-    ]
+import Header from './components/Header'
+import FeedbackItem from './components/FeedbackItem'
 
-    const loading = false
-    const showComments = true
-    const commentBlock = (<div className='comments'>
-    <h3>Comments ({comments.length})</h3>
-        <ul>
-            {comments.map((comment, index) =>
-            <li key={index}>{comment.text}</li>
-            )}
-        </ul>
-</div>)
-
-    if(loading) return <h1>Loading. . .</h1>
-
+function App() {
     return (
-        <div className='container'>
-            <h1>{title.toUpperCase()}</h1>
-            <p>{body}</p>
-
-            {showComments && (commentBlock)}
+    <>
+        <Header />
+        <div className="container">
+            <FeedbackItem />
         </div>
-    )
+    </>
+)
 }
 
 export default App
