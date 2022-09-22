21610f385f87e525727334f7e902c708c8a27923
terickson67 <terickson@cyberhusky.io>
2022-09-22 15:41:00 -0400

    FeebackList commit

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,18 @@
+import FeedbackItem from './FeedbackItem'
+
+function FeedbackList({feedback}) {
+  console.log(feedback)
+    if(!feedback || feedback.length === 0){
+    return <p>No Feedback Yet</p>
+  }
+
+    return (
+    <div className="feedback-list">
+        {feedback.map((item) => (
+            <FeedbackItem  key={item.id} item={item}/>
+        ))}
+    </div>
+  )
+}
+
+export default FeedbackList
