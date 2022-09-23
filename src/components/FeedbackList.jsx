de34a1ff6d7f65bc8c4262704898c02b39cf66d0
terickson67 <terickson@cyberhusky.io>
2022-09-23 15:35:57 -0400

    afternoon work 9/23

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,32 +1,46 @@
-import PropTypes from 'prop-types'
+import { motion, AnimatePresence } from 'framer-motion'
+import { useContext } from 'react'
 import FeedbackItem from './FeedbackItem'
+import Spinner from './shared/Spinner'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackList({feedback, handleDelete}) {
-    if(!feedback || feedback.length === 0){
+// NOTE: added layout prop for nicer animation
+// https://www.framer.com/docs/animation/#layout-animations
+
+function FeedbackList() {
+  const { feedback, isLoading } = useContext(FeedbackContext)
+
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet</p>
   }
 
-    return (
-    <div className="feedback-list">
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <div className='feedback-list'>
+      <AnimatePresence>
         {feedback.map((item) => (
-            <FeedbackItem 
-             key={item.id} 
-             item={item} 
-             handleDelete={handleDelete} 
-             />
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            layout
+          >
+            <FeedbackItem key={item.id} item={item} />
+          </motion.div>
         ))}
+      </AnimatePresence>
     </div>
   )
-}
 
-FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired,
-    })
-  )
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((item) => (
+  //       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // )
 }
 
 export default FeedbackList
--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,20 +1,28 @@
 import PropTypes from 'prop-types'
 
-function Card({children, reverse}) {
+function Card({ children, reverse }) {
+  // return <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+
   return (
-    <div>
-      <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+    <div
+      className='card'
+      style={{
+        backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#fff',
+        color: reverse ? '#fff' : '#000',
+      }}
+    >
+      {children}
     </div>
   )
 }
 
 Card.defaultProps = {
-    reverse: false
-  }
+  reverse: false,
+}
 
-  Card.propTypes = {
-    children: PropTypes.node.isRequired,
-    reverse: PropTypes.bool,
-  }
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  reverse: PropTypes.bool,
+}
 
 export default Card
